46bfb32c36c3eaf852b058234d61d3f935d78738
Arthur Chabole <arthur.chabole@unesp.br>
2021-05-16 21:50:30 -0300

    Update classe_asa.py

--- Projeto Zebra/classe_asa.py
+++ Projeto Zebra/classe_asa.py
@@ -22,9 +22,9 @@ class Wing:
         
         self.AR = (self.b**2)/self.S
     
-    def V_stall(self):
+    def V_stall(self, rho):
         self.rho = rho
-        return (2*self.W/(self.rho*self.S*self.CLmax))**0.5
+        return (2*self.W/(rho*self.S*self.CLmax))**0.5
 
         
 Zb_1 = Wing('Asa 1', 0.988, 2.08, 1.7)
@@ -34,7 +34,7 @@ Zb_3 = Wing('Asa 3', 2, 3, 1.95)
 asas = np.array((Zb_1, Zb_2, Zb_3))
 We = np.array((140, 150, 160))
 
-
+Zb_1.V_stall()
 
 
 
